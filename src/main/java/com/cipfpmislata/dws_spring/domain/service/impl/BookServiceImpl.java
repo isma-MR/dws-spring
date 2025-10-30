b54ca98bd6d486d6f715fe9564d7979c6aa6922b
Ismael Monleón Rubio <ismael.monleon73@gmail.com>
2025-10-30 10:20:10 +0100

    Added new book

--- src/main/java/com/cipfpmislata/dws_spring/domain/service/impl/BookServiceImpl.java
+++ src/main/java/com/cipfpmislata/dws_spring/domain/service/impl/BookServiceImpl.java
@@ -15,7 +15,8 @@ public class BookServiceImpl implements BookService {
             new Book("Book 1", "ISBN001", List.of(), null),
             new Book("Book 2", "ISBN002", List.of(), null),
             new Book("Book 3", "ISBN003", List.of(), null),
-            new Book("Book 4", "ISBN004", List.of(), null)
+            new Book("Book 4", "ISBN004", List.of(), null),
+            new Book("Book 5", "ISBN005", List.of(), null)
     );
     @Override
     public List<Book> getAll() {
